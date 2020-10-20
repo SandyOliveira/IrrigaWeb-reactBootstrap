d3bcdbd298e397f9a0b8f5d4a2dfc8115d714789
SandyOliveira <sandyoliveira828@gmail.com>
2020-10-20 17:24:12 -0300

    history funcionando

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -4,6 +4,7 @@ import { Route, Redirect } from "react-router-dom";
 
 import AuthLayout from "../pages/_layouts/auth";
 import DefaultLayout from "../pages/_layouts/default";
+import store from "../store";
 
 export default function RouteWrapper({
   component: Component,
@@ -33,7 +34,7 @@ export default function RouteWrapper({
     };
   }, []);
 
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
